8b23b544bad85b499d60223d18cf09dfbaa1c255
Robbie <mr.robbie.mcgregor@gmail.com>
2021-09-01 14:30:00 +1200

    public folder configured

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const app = express()
+app.use(express.static(__dirname + '/public'));
 let port = process.env.PORT || 80
 
-app.get("*", (req, res) => {
-    res.sendFile(__dirname + req.url)
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname + req.url))
 })
 
 app.listen(port, () => {
