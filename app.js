47f667abfd902b921b0985f18ece7e2de2876afc
Robbie <mr.robbie.mcgregor@gmail.com>
2021-09-05 11:52:19 +1200

    https redirect added

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ app.use(express.static(process.env.PWD + '/public'));
 // app.use(express.static(__dirname + '/public'));
 let port = process.env.PORT || 80
 
+app.enable('trust proxy')
+app.use((req, res, next) => {
+    req.secure ? next() : res.redirect('https://' + req.headers.host + req.url)
+})
+
+
+
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname + `/${req.url}`))
 })
