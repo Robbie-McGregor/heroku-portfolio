e7a7269b93e38c3df4b5c5f6e1663fdf8f3e84d4
Robbie <mr.robbie.mcgregor@gmail.com>
2021-09-05 12:01:36 +1200

    https redirect removed - not working

--- app.js
+++ app.js
@@ -7,19 +7,6 @@ app.use(express.static(process.env.PWD + '/public'));
 // app.use(express.static(__dirname + '/public'));
 let port = process.env.PORT || 80
 
-app.enable('trust proxy')
-
-
-app.use((req, res, next) => {
-        // perform host checking prior to https checking, by the way
-        if (req.headers.host === 'robbie-mcgregor.herokuapp.com')
-            // make express use your custom domain name instead of heroku's default
-            return res.redirect(301, 'https://www.robbie-mcgregor.com');
-        if (req.headers['x-forwarded-proto'] !== 'https')
-            return res.redirect('https://' + req.headers.host + req.url);
-        else
-            return next();
-});
 
 
 
